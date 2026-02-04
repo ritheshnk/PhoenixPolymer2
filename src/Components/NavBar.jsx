47ab3beeb55b831af7546284842d75cfd0abc579
Rithesh NK <61980882+ritheshnk@users.noreply.github.com>
2026-02-05 00:42:41 +0530

    Add NavBar component with placeholder links
    Add a new React functional component at src/Components/NavBar.jsx that renders a basic <nav> with an unordered list of placeholder items (Home, About, Products, Contact). Exported as default; intended as a stub to be wired up with routing and styling later.

---
+++ src/Components/NavBar.jsx
@@ -0,0 +1,16 @@
+export default function NavBar() {
+    return (
+        <>
+            <div>
+                <nav>
+                    <ul>
+                        <li>Home</li>
+                        <li>About</li>
+                        <li>Products</li>
+                        <li>Contact</li>
+                    </ul>
+                </nav>
+            </div>
+        </>
+    )
+}
